c852c5cb1a7856ba59e1c2da7b55aaca3921feff
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-02 22:02:15 +0800

    Return the evaluation of the score.

--- prac_02/score.py
+++ prac_02/score.py
@@ -6,3 +6,10 @@ def main():
     random_score = random.randint(0, 100)
     print(f"Random score evaluation: {random_score} is {evaluate_score(random_score)}")
 
+def evaluate_score(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"

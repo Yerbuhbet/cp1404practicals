8d4f3c937aac89501dd6b35fb8f08793589f909b
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-02 22:17:17 +0800

    Prompt user until they enter a valid score between 0 and 100.

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -11,3 +11,9 @@ def main():
         else:
             print("Invalid option")
         choice = display_menu()
+def get_valid_score():
+    score = float(input("Enter a valid score (0-100): "))
+    while score < 0 or score > 100:
+        print("Invalid score. The score must be between 0 and 100.")
+        score = float(input("Enter a valid score (0-100): "))
+    return score

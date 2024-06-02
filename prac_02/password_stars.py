2ad9a27d3abc90c9a7b7272216078748b885fffa
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-02 21:26:01 +0800

    Asking user for password

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,4 +1,7 @@
+MIN_LENGTH = 8
 def main():
-    MIN_LENGTH = 8
     password = get_password(MIN_LENGTH)
     print('*' * len(password))
+
+def get_password(MIN_LENGTH):
+    password = input("Enter your password: ")

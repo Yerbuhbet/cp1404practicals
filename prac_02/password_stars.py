f461a952dc9ca1f8c4fc6c03ce3d8b574c9d4e3b
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-02 21:49:47 +0800

    Prompt user for a password, return the password if it meets the criteria.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,3 +1,11 @@
 def main():
     password = get_password()
     print_asterisks(password)
+
+def get_password():
+    min_length = 8
+    password = input("Enter your password: ")
+    while len(password) < min_length:
+        print(f"Password must be at least {min_length} characters long.")
+        password = input("Enter your password: ")
+    return password

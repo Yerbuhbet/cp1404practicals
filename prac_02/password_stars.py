f7fe66b56512b37fa1d278c02c437c8669905c04
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-02 21:28:51 +0800

    Recursively ask for the password again if it's too short

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -5,3 +5,7 @@ def main():
 
 def get_password(MIN_LENGTH):
     password = input("Enter your password: ")
+    if len(password) < MIN_LENGTH:
+        print(f"Password must be at least {MIN_LENGTH} characters long. Please try again.")
+        return get_password(MIN_LENGTH)
+    return password

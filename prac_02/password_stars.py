ffe6b226961cfd3f450e3f56a33932566b662004
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-02 21:29:14 +0800

    Testing

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -9,3 +9,4 @@ def get_password(MIN_LENGTH):
         print(f"Password must be at least {MIN_LENGTH} characters long. Please try again.")
         return get_password(MIN_LENGTH)
     return password
+main()

28d1e58977cfdef7224b68520d8f1950504cac50
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-09 23:36:27 +0800

    Add functionality to write user's name to 'name.txt'

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,25 +1,5 @@
-# Setup empty function structures for file operations
-
-def write_name():
-    out_file = open("name.txt", "w")
-    name = input("What is your name? ")
-    print(name, file=out_file)
-    out_file.close()
-
-
-def read_name():
-    pass
-
-def read_name_with():
-    pass
-
-def sum_first_two_numbers():
-    pass
-
-def sum_all_numbers():
-    pass
-
-def main():
-    pass
-
-main()
+# Quick Program 1
+out_file = open("name.txt", "w")
+name = input("What is your name? ")
+print(name, file=out_file)  # or out_file.write(name)
+out_file.close()

ef0a78268f96f609fac46a641e16e724c719a10a
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-09 21:49:22 +0800

    Implement stock price simulation logic

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -18,3 +18,18 @@ INITIAL_PRICE = 10.0
 FILENAME = "stock_output.txt"
 # open output file for writing (this creates a new file if it doesn't exist)
 out_file = open(FILENAME, 'w')
+
+price = INITIAL_PRICE
+day = 0
+print(f"Starting price: ${price:,.2f}", file=out_file)
+
+while MIN_PRICE <= price <= MAX_PRICE:
+    price_change = 0
+    day += 1
+    if random.randint(1, 2) == 1:
+        price_change = random.uniform(0, MAX_INCREASE)
+    else:
+        price_change = random.uniform(-MAX_DECREASE, 0)
+
+    price *= (1 + price_change)
+    print(f"On day {day} price is: ${price:,.2f}", file=out_file)

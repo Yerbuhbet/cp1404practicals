4dd16a8720e9977c242a2073e9500aa912d2d2f5
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-09 23:36:54 +0800

    Implement reading of name from 'name.txt' and display it

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,3 +3,8 @@ out_file = open("name.txt", "w")
 name = input("What is your name? ")
 print(name, file=out_file)  # or out_file.write(name)
 out_file.close()
+# Quick Program 2
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+in_file.close()
+print("Your name is", name)

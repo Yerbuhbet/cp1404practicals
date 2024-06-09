f4430edc39b1413465830d392cea61aba18e61cc
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-09 20:54:08 +0800

    Implement random number generation examples using randint, randrange, and uniform

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -3,3 +3,7 @@ print(dir(random))
 
 help(random.randint)
 help(random.randrange)
+
+print("Random integer between 5 and 20:", random.randint(5, 20))  # line 1
+print("Random number from range 3 to 10 step 2:", random.randrange(3, 10, 2))  # line 2
+print("Random floating point number between 2.5 and 5.5:", random.uniform(2.5, 5.5))  # line 3

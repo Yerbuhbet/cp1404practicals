e1b3dd92ceabacbec0d7a5545892950289a82d30
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-09 22:40:21 +0800

    Setup initial function structures for file operations

---
+++ prac_03/files.py
@@ -0,0 +1,21 @@
+# Setup empty function structures for file operations
+
+def write_name():
+    pass
+
+def read_name():
+    pass
+
+def read_name_with():
+    pass
+
+def sum_first_two_numbers():
+    pass
+
+def sum_all_numbers():
+    pass
+
+def main():
+    pass
+
+main()

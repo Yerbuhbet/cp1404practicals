a333fb5131445b8f20ba5ee0b214916c12ae24a3
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-09 22:07:10 +0800

    Complete loop with exit condition upon valid input

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -7,7 +7,7 @@ is_finished = False
 while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
-        # TODO: this line
+        is_finished = True  # Sets the loop to finish if input is successfully converted
     except ValueError:  # Catches the ValueError which occurs if the input isn't a valid integer
         print("Please enter a valid integer.")
 print("Valid result is:", result)

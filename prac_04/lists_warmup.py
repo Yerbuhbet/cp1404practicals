466eb7aa91c3c4c1e195275e43fd06964aedb149
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-16 20:26:33 +0800

    Initialize list and add predictions for element access and membership tests

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -8,3 +8,6 @@ print(5 in numbers) # True
 print(7 in numbers) # False
 print("3" in numbers) # False, because "3" is a string and the list contains integers
 print(numbers + [6, 5, 3]) # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+# Change the first and last elements in the list
+numbers[0] = "ten"  # Change first element to 'ten'
+numbers[-1] = 1     # Change last element to

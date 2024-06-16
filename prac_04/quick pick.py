2bc5bd1f8aa7ded8dada6317e034f66974def3e6
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-16 21:19:27 +0800

    Add main function to prompt user input and print quick pick tickets.

--- prac_04/quick pick.py
+++ prac_04/quick pick.py
@@ -4,3 +4,8 @@ import random
 NUMBERS_PER_LINE = 6
 MIN_NUMBER = 1
 MAX_NUMBER = 45
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    for i in range(number_of_quick_picks):
+        quick_pick = generate_quick_pick()
+        print(" ".join(f"{number:2}" for number in sorted(quick_pick)))

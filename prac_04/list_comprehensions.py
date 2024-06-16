37608d3e02c8b4d5b636e35f49f75fbc14cb8904
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-16 21:11:10 +0800

    Implement list comprehension to convert full names to lowercase

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -11,3 +11,6 @@ print(full_initials)
 # List comprehension to select names starting with 'A'
 a_names = [name for name in names if name.startswith('A')]
 print(a_names)
+# List comprehension to convert all full names to lowercase
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)

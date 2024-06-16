bc8ae1e158dd3f7bc0672b541e91a0e24c56007c
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-16 20:42:40 +0800

    Add functionality to collect monthly incomes

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -10,12 +10,3 @@ def main():
     for month in range(1, months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
-
-    print("\nIncome Report\n-------------")
-    total = 0
-    for month in range(1, months + 1):
-        income = incomes[month - 1]
-        total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
-
-    main()

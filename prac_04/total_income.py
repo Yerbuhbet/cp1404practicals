3b024ca1415ecc6d7e1c73ad58919f90342bf77c
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-16 20:43:11 +0800

    Implement income report generation and display

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -10,3 +10,12 @@ def main():
     for month in range(1, months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
+
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, months + 1):
+        income = incomes[month - 1]
+        total += income
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
+main()

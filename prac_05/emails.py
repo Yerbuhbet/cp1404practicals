f54bbd79b8127bc60a90682a2fb0b08c994707cb
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-23 23:26:19 +0800

    Add confirmation step for extracted names

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,6 +2,11 @@ def main():
     email_to_name = {}
     email = input("Email: ")
     while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ")
+        if confirmation.lower() not in ('y', 'yes', ''):
+            name = input("Name: ")
+        email_to_name[email] = name
         email = input("Email: ")
 def get_name_from_email(email):
     prefix = email.split('@')[0]

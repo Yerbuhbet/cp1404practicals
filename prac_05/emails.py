a1b5e7a69fc33d131adce424a1a97234dec87c61
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-23 23:28:04 +0800

    Final Code with Comments Indicating Commits

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,17 +2,17 @@ def main():
     email_to_name = {}
     email = input("Email: ")
     while email != "":
-        name = get_name_from_email(email)
-        confirmation = input(f"Is your name {name}? (Y/n) ")
+        name = get_name_from_email(email)  # Commit 2
+        confirmation = input(f"Is your name {name}? (Y/n) ")  # Commit 3
         if confirmation.lower() not in ('y', 'yes', ''):
             name = input("Name: ")
         email_to_name[email] = name
         email = input("Email: ")
 
-    for email, name in email_to_name.items():
+    for email, name in email_to_name.items():  # Commit 4
         print(f"{name} ({email})")
 
-def get_name_from_email(email):
+def get_name_from_email(email):  # Commit 2
     prefix = email.split('@')[0]
     parts = prefix.split('.')
     name = " ".join(parts).title()

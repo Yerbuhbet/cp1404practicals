db4141a05ec50ca3705dae2132861b6502480b76
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-23 23:18:19 +0800

    Implement hex code lookup with controlled loop and exit condition

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -10,3 +10,16 @@ HEX_COLOUR_CODES = {
     "Azure1": "#f0ffff",
     "Azure2": "#e0eeee"
 }
+def main():
+    print("Hex Colour Lookup")
+    colour_name = input("Enter a colour name (or leave blank to exit): ")
+    while colour_name:
+        colour_name = colour_name.capitalize()
+        if colour_name in HEX_COLOUR_CODES:
+            print(f"The code for {colour_name} is {HEX_COLOUR_CODES[colour_name]}")
+        else:
+            print("Invalid colour name")
+        colour_name = input("Enter another colour name (or leave blank to exit): ")
+
+
+    main()

0499fd8b046276bf29bdc653c86582e579a00b6f
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-23 23:27:36 +0800

    Implement final output of emails and associated names

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,8 +8,14 @@ def main():
             name = input("Name: ")
         email_to_name[email] = name
         email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
 def get_name_from_email(email):
     prefix = email.split('@')[0]
     parts = prefix.split('.')
     name = " ".join(parts).title()
     return name
+
+main()

8057bae5c698e42049a5b096303e1875731c8f82
Yerbuhbet <akbet.ereke@gmail.com>
2024-06-23 23:40:06 +0800

    Implement data processing for champions and countries

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -11,9 +11,21 @@ def main():
 def read_data(filename):
     with open(filename, "r", encoding="utf-8-sig") as file:
         reader = csv.reader(file)
-        next(reader)  
+        next(reader)
         data = [line for line in reader]
     return data
 
+def process_data(data):
+    champions = {}
+    countries = set()
+    for row in data:
+        champion = row[2].strip()
+        country = row[1].strip()
+        countries.add(country)
+        if champion in champions:
+            champions[champion] += 1
+        else:
+            champions[champion] = 1
+    return champions, sorted(countries)
 
 main()

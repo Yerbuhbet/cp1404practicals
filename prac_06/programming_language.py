bb8fa8c6017bd363682b359f0a8aa3df0911ee89
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-14 22:03:36 +0800

    Initialize ProgrammingLanguage class with constructor

---
+++ prac_06/programming_language.py
@@ -0,0 +1,15 @@
+"""CP1404/CP5632 Practical - ProgrammingLanguage class example.
+
+Estimate: 30 minutes
+Start time: 19:34 (47min)
+"""
+
+class ProgrammingLanguage:
+    """Represent a ProgrammingLanguage object."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year

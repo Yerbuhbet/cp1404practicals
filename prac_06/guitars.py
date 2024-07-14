27549c0a094905a851c7f31919817d4e35059816
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-14 22:20:39 +0800

    Display list of guitars with detailed information

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -16,5 +16,10 @@ def main():
         print(f"{guitar} added.")
         name = input("\nName: ")
 
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
 if __name__ == '__main__':
     main()

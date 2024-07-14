25fe2849c410432ebc59f0aa72a44e8458a25995
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-14 21:53:50 +0800

    Initialize Car class with constructor

---
+++ prac_06/car.py
@@ -0,0 +1,15 @@
+"""CP1404/CP5632 Practical - Car class example."""
+
+
+class Car:
+    """Represent a Car object."""
+
+    def __init__(self, name="", fuel=0):
+        """Initialise a Car instance.
+
+        name: string, the name of the car
+        fuel: float, one unit of fuel drives one kilometre
+        """
+        self.name = name
+        self.fuel = fuel
+        self._odometer = 0

c4d091f687abac7f811bb173f43a435001e6eba5
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-14 22:17:26 +0800

    Implement test cases for Guitar class methods

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,3 +1,17 @@
 """CP1404/CP5632 Practical - Guitar class testing."""
 
 from prac_06.guitar import Guitar
+
+def main():
+    """Test Guitar class methods."""
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another = Guitar("Another Guitar", 2013, 500)
+
+    print(f"{gibson.name} get_age() - Expected 100. Got {gibson.get_age()}")
+    print(f"{another.name} get_age() - Expected 9. Got {another.get_age()}")
+
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another.name} is_vintage() - Expected False. Got {another.is_vintage()}")
+
+if __name__ == '__main__':
+    main()

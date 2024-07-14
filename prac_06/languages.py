d826dcdf4e0216a05e5d72a64ff52a38f674e4bd
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-14 22:08:08 +0800

    Create and print instances of ProgrammingLanguage

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,3 +2,17 @@
 
 from prac_06.programming_language import ProgrammingLanguage
 
+def main():
+    """Create instances of ProgrammingLanguage and display their details."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    java = ProgrammingLanguage("Java", "Static", True, 1995)
+    cpp = ProgrammingLanguage("C++", "Static", False, 1983)
+
+    languages = [python, ruby, visual_basic, java, cpp]
+
+    print(f"{python}\n{ruby}\n{visual_basic}\n{java}\n{cpp}")
+
+if __name__ == '__main__':
+    main()

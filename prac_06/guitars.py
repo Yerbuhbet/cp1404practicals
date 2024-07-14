a20a17a132f0e4a6a9ca1b5f9216e48750be6f32
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-14 22:20:19 +0800

    Implement interactive guitar addition in main function

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,3 +1,20 @@
 """CP1404/CP5632 Practical - Guitar program."""
 
 from prac_06.guitar import Guitar
+
+def main():
+    """Manage a collection of guitars."""
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name:
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        name = input("\nName: ")
+
+if __name__ == '__main__':
+    main()

cd1856b5c1af5c2794b61bb886484bb45ec576cf
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-14 22:12:56 +0800

    Initialize Guitar class with constructor

---
+++ prac_06/guitar.py
@@ -0,0 +1,14 @@
+"""CP1404/CP5632 Practical - Guitar class example.
+
+Estimate: 45 minutes
+Start time: [record your start time here]
+"""
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost

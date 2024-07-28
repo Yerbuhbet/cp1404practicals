cb8c0d8a1834f7c15fa2e3804feccc2795d0db5f
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-28 22:46:09 +0800

    Refactor code and add comments

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,9 +1,24 @@
 # squaring.py
+"""
+Pseudocode:
+class SquareNumberApp(App)
+    function build(self)
+        Window.size = (200, 100)
+        self.title = "Square Number"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+    function handle_calculate(self, value)
+        try
+            result = float(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError
+            pass
+"""
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-
-__author__ = 'Lindsay Ward'
 
 
 class SquareNumberApp(App):

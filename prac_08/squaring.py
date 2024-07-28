b62fa3993670345801a9f50b3bac4a596595fdcf
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-28 22:45:32 +0800

    Add calculate functionality

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,11 +1,4 @@
 # squaring.py
-"""
-CP1404/CP5632 Practical
-Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-Started 13/10/2015
-"""
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window

4e6417f0a20f33e49eb3b84491d97be8c85c6771
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-28 22:33:35 +0800

    Add dynamic labels creation functionality

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,11 +1,17 @@
-# dynamic_labels.py
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
 class DynamicLabelsApp(App):
     def build(self):
-        return BoxLayout(orientation='vertical')
+        names = ["Yermukhamet Akbet", "Shafa", "Aruzhan", "Julia", "Bota", "Dastan"]
+        main_layout = BoxLayout(orientation='vertical')
+
+        for name in names:
+            label = Label(text=name, font_size=20)
+            main_layout.add_widget(label)
+
+        return main_layout
 
 if __name__ == '__main__':
     DynamicLabelsApp().run()

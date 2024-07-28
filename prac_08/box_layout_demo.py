961f06544ca903ccf3a8e0d15bc2bd882e339b8c
Yerbuhbet <akbet.ereke@gmail.com>
2024-07-28 21:22:47 +0800

    Added handle_greet function to update output_label text with a greeting.

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -7,6 +7,7 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
-
+    def handle_greet(self):
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
 BoxLayoutDemo().run()

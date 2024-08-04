200e17c80016135bd7f0fe81b472ef64e789c51d
Yerbuhbet <akbet.ereke@gmail.com>
2024-08-04 19:30:09 +0800

    Implement reliability-based driving in UnreliableCar

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,5 +1,5 @@
 from car import Car
-
+import random
 class UnreliableCar(Car):
     """Represent a car that may or may not drive based on its reliability."""
 
@@ -7,3 +7,10 @@ class UnreliableCar(Car):
         """Initialise an UnreliableCar instance."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Attempt to drive the car a given distance based on its reliability."""
+        if random.random() * 100 < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0

fe7ed06d3e2c8781c1e0a670aa853352af99a7ed
Yerbuhbet <akbet.ereke@gmail.com>
2024-08-04 19:39:06 +0800

    Add instantiation and print initial state for SilverServiceTaxi in test script

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,7 +1,10 @@
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
 def test_silver_service_taxi():
-    print("Starting test for SilverServiceTaxi...")
+    # Create an instance of SilverServiceTaxi
+    luxury_taxi = SilverServiceTaxi("Hummer", 200, 2)
+    print("Initial state of SilverServiceTaxi:", luxury_taxi)
+
 
 if __name__ == "__main__":
     test_silver_service_taxi()

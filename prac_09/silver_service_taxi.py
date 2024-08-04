8a89c0a2ffbda0f8313467c9568911e1b641a414
Yerbuhbet <akbet.ereke@gmail.com>
2024-08-04 19:35:08 +0800

    Add flagfall to SilverServiceTaxi and override fare calculation

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,9 +2,14 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Represent a luxury version of a Taxi with additional fare costs."""
+    flagfall = 4.50  # Class variable for the additional charge on each new fare
 
     def __init__(self, name, fuel, fanciness):
         """Initialise a SilverServiceTaxi instance."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * self.fanciness  # Multiply base price by fanciness level
+
+    def get_fare(self):
+        """Calculate and return the price for the taxi trip including flagfall."""
+        return super().get_fare() + self.flagfall

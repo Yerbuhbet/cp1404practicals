92ea876991791027e3e7ac26f224614c71816795
Yerbuhbet <akbet.ereke@gmail.com>
2024-08-04 19:35:51 +0800

    Update string representation for SilverServiceTaxi to include flagfall

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -13,3 +13,7 @@ class SilverServiceTaxi(Taxi):
     def get_fare(self):
         """Calculate and return the price for the taxi trip including flagfall."""
         return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        """Return a string representation of the SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"

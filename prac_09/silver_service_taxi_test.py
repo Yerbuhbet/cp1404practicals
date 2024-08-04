75082f77efd625340f6a89d21b6ea0c40c676f68
Yerbuhbet <akbet.ereke@gmail.com>
2024-08-04 19:37:41 +0800

    Set up basic structure for testing SilverServiceTaxi

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,7 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+def test_silver_service_taxi():
+    print("Starting test for SilverServiceTaxi...")
+
+if __name__ == "__main__":
+    test_silver_service_taxi()
